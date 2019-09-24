2dec6f285b39668e7684ed455a678854aee67716
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-09-24 19:23:38 +0700

    Initial commit

---
+++ src/com/pbo/PBO11K10118902Latihan15Logika.java
@@ -0,0 +1,23 @@
+package com.pbo;
+
+/**
+ *
+ * #Author
+ * Nama : Agung Sepruloh
+ * Kelas : PBO11K
+ * NIM : 10118902
+ *
+ */
+
+public class PBO11K10118902Latihan15Logika {
+
+    public static void main(String[] args) {
+	    boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a  = " + (!a));
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+}
